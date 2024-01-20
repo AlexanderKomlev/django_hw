8f57c69fb85653cea368318dbbba14d0145af158
Alexander Komlev <alexander.komlev42@gmail.com>
2024-01-20 11:25:13 +0700

    fix

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,6 +1,3 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-
 from rest_framework import generics
 from .models import Sensor, Measurement
 from .serializers import SensorSerializer, SensorDetailSerializer
